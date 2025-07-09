240fef767b9e1dec9c031ea27ffdc5a99e6ce87d
Chrystine Martins <126017173+Chrysthy@users.noreply.github.com>
2025-07-09 08:45:39 -0300

    add preventDefault and setLoading function

--- src/js/script.js
+++ src/js/script.js
@@ -1,6 +1,20 @@
 document.addEventListener('DOMContentLoaded', () => {
 
-    const form = document.querySelector('.form')
-  
+    const form = document.querySelector('.form');
+    const btn = document.querySelector('.btn');
 
-})
+    form.addEventListener("submit", (event) => {
+        event.preventDefault();
+
+        const description = document.getElementById("description").value;
+    });
+
+    function setLoading(isLoading) {
+
+        btn.innerHTML = isLoading ? "Gerando Background..." : "Gerar Background";
+
+    }
+
+
+
+});

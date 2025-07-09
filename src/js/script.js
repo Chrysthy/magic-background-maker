5da7080670371de17f770bc89dc04fb3360ef92b
Chrystine Martins <126017173+Chrysthy@users.noreply.github.com>
2025-07-09 15:00:28 -0300

    add new consts and changing data

--- src/js/script.js
+++ src/js/script.js
@@ -2,21 +2,35 @@ document.addEventListener('DOMContentLoaded', () => {
 
     const form = document.querySelector('.form');
     const btn = document.querySelector('.btn');
+    const htmlCode = document.querySelector('.html-code');
+    const cssCode = document.querySelector('.css-code');
+    const preview = document.querySelector('.preview-area')
 
-    form.addEventListener("submit", (event) => {
+    form.addEventListener("submit", async event => {
         event.preventDefault();
 
         const description = document.getElementById("description").value;
 
         try {
-            const response = fetch("", 
+            const response = await fetch("", {
                 method: "POST",
-                headers: {"Content-Type": "application/json"},
-                body: JSON.stringify({description})            )
-            
+                headers: { "Content-Type": "application/json" },
+                body: JSON.stringify({ description })
+            });
+
+            const data = await response.json();
+
+            htmlCode.textContent = data.code;
+            cssCode.textContent = data.style;
+
+            preview.style.display = "block";
+            preview.innerHTML = data.code;
+
+
+
         } catch (error) {
             console.error("Erro ao obter o valor do campo de texto:", error);
-            
+
         } finally {
 
         }
